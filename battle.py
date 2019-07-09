0a0dd65c50ca52e82d43897046d9aa73ce33155f
jeff-lund <jdl3@pdx.edu>
2019-07-09 14:57:24 -0700

    adding gamestate definition file

--- battle.py
+++ battle.py
@@ -5,49 +5,25 @@ from pygame.locals import *
 import sys
 from cards import Card, Hand
 from character import *
+from gamestate import *
 
-ASSETS_PATH = os.path.join(os.getcwd(), 'assets')
-SHIPS_PATH = os.path.join("Spaceships", "spaceships", "parts_spriter_animation")
-CARD_PATH = os.path.join(ASSETS_PATH, "Cards")
-# declare the size of the map
-WIDTH = 1400
-HEIGHT = 1000
-CARD__WIDTH = 93
-CARD_HEIGHT = 130
-
-white = (200, 200, 200) 
-green = (0, 200, 0) 
-blue = (0, 0, 200)
-black = (0,0,0)
-gray = (128,128,128)
-
-bright_white = (255,255,255)
-bright_green = (0, 255, 0)
-bright_blue = (0, 0, 255)
-bright_gray = (169,169,169)
-
-bg = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_PATH, "Background", "spacefield_a-000.png")), (WIDTH, HEIGHT))
-pg = Player()
-player = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship3/Ship3.png'), 100, 500)
-pg.add(player)
-
-def targeting(screen, bg, pg, eg, hand):
+def targeting(screen, bg, player_group, enemy_group, hand):
     pygame.mouse.set_visible(False)
     target = Target()
     targeted = False
     while pygame.mouse.get_pressed()[0]:
         screen.blit(bg, (0, 0))
         hand.draw_hand(screen)
-        pg.draw(screen)
-        eg.draw(screen)
-        eg.draw_rect(screen)
-        pg.draw_rect(screen)
+        player_group.draw(screen)
+        enemy_group.draw(screen)
+        enemy_group.draw_rect(screen)
+        player_group.draw_rect(screen)
         mX, mY = pygame.mouse.get_pos()
         target.update(mX, mY)
-        #if any(pygame.sprite.spritecollide(target, eg, False)):
-        if any([sp.collision(mX, mY) for sp in eg]):
+        #if any(pygame.sprite.spritecollide(target, enemy_group, False)):
+        if any([sp.collision(mX, mY) for sp in enemy_group]):
             screen.blit(target.get_atk(), (mX, mY))
-        elif any([sp.collision(mX, mY) for sp in pg]):
+        elif any([sp.collision(mX, mY) for sp in player_group]):
             screen.blit(target.get_bst(), (mX, mY))
         else:
             screen.blit(target.get_img(), (mX, mY))
@@ -56,32 +32,39 @@ def targeting(screen, bg, pg, eg, hand):
     pygame.mouse.set_visible(True)
     return
 
-def battle(screen, pg):
-    eg = Enemy()
-    enemy0 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship4/Ship4.png'), 900, 300)
-    enemy1 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship2/Ship2.png'), 800, 450)
-    enemy2 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship5/Ship5.png'), 900, 650)
-    eg.add(enemy0, enemy1, enemy2)
-    for e in eg:
-        e.flip()
-    eg.update()
-    pg.update()
+def battle(screen, player_group):
+    bg = pygame.transform.scale(pygame.image.load(
+            os.path.join(ASSETS_PATH, "Background", "spacefield_a-000.png")),
+            (SCREEN_WIDTH, SCREEN_HEIGHT))
+    enemy_group = Enemy()
+    # change to dynamically create enemies
+    enemy0 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship4/Ship4.png'),
+        SCREEN_WIDTH - 100, SCREEN_HEIGHT / 2)
+    enemy1 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship2/Ship2.png'),
+        SCREEN_WIDTH - 200, SCREEN_HEIGHT / 2)
+    enemy2 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship5/Ship5.png'),
+        SCREEN_WIDTH - 100, SCREEN_HEIGHT / 2)
+    enemy_group.add(enemy0, enemy1, enemy2)
+    for enemy in enemy_group:
+        enemy.flip()
+    enemy_group.update()
+
+    player_group.update()
     hand = Hand()
-    hand.add(Card("C_Laser_Cannon.png", WIDTH / 2 - CARD__WIDTH / 2, HEIGHT - CARD_HEIGHT),
-             Card("C_Shield.png", WIDTH / 2 + CARD__WIDTH / 2, HEIGHT - CARD_HEIGHT))
+    # change to draw function
+    hand.add(Card("C_Laser_Cannon.png", SCREEN_WIDTH / 2 - CARD__WIDTH / 2, SCREEN_HEIGHT - CARD_HEIGHT),
+             Card("C_Shield.png", SCREEN_WIDTH / 2 + CARD__WIDTH / 2, SCREEN_HEIGHT - CARD_HEIGHT))
     hand.update()
     pygame.mouse.set_visible(True)
 
     while True:
+        # draw background
         screen.blit(bg, (0, 0))
+        # dynamic hand animation
         hand.ddraw(screen, pygame.mouse.get_pos())
         # place the player object (the loaded image)
-        for p in pg:
-            screen.blit(p.get_img(), player.pos)
-        for e in eg:
-            screen.blit(e.get_img(), e.pos)
-            screen.blit(e.get_img(), e.pos)
-            screen.blit(e.get_img(), e.pos)
+        player_group.draw(screen)
+        enemy_group.draw(screen)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -91,10 +74,22 @@ def battle(screen, pg):
                 for card in hand:
                     if card.rect.collidepoint(pygame.mouse.get_pos()):
                         card.highlight = True
-                        targeting(screen, bg, pg, eg, hand)
+                        targeting(screen, bg, player_group, enemy_group, hand)
                         card.highlight = False
                         break
 
             # update the display every iteration of this loop
             pygame.display.update()
 
+if __name__ == "__main__":
+    # Run battle.py directly to test battle functionality
+    pygame.init()
+    pygame.display.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # declare the size of the map
+
+    player_group = Player()
+    player = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship3/Ship3.png'),
+        50, SCREEN_HEIGHT / 4)
+    player_group.add(player)
+    battle(screen, player_group)
---
+++ gamestate.py
@@ -0,0 +1,23 @@
+import os
+
+# DEFINITIONS
+WHITE = (200, 200, 200)
+GREEN = (0, 200, 0)
+BLUE = (0, 0, 200)
+BLACK = (0,0,0)
+GRAY = (128,128,128)
+
+BRIGHT_WHITE = (255,255,255)
+BRIGHT_GREEN = (0, 255, 0)
+BRIGHT_BLUE = (0, 0, 255)
+BRIGHT_GRAY = (169,169,169)
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 512
+CARD__WIDTH = 93
+CARD_HEIGHT = 130
+
+# PATHS
+ASSETS_PATH = os.path.join(os.getcwd(), 'assets')
+SHIPS_PATH = os.path.join(ASSETS_PATH, "Spaceships", "spaceships", "parts_spriter_animation")
+CARD_PATH = os.path.join(ASSETS_PATH, "Cards")
