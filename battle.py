5a93774f663a2b276e03884ec4597aa7945875fc
jeff-lund <jdl3@pdx.edu>
2019-07-13 15:12:58 -0700

    moved card images into image subdir in Cards. created json card file and json loading function. Modified cards to be positioned by screen height/width

--- battle.py
+++ battle.py
@@ -3,11 +3,11 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pygame.locals import *
 import sys
-from cards import Card, Hand
-from character import Player, Enemy, Character, Target
+import cards
+from character import Player, Enemy, Enemy_Gr, Character, Target
 from gamestate import *
 
-def targeting(screen, bg, player_group, enemy_group, hand):
+def targeting(screen, bg, card, player, enemy_group):
     '''
     Function called when a card is selected by the player
     Sucessful targeting means the card is used
@@ -19,19 +19,19 @@ def targeting(screen, bg, player_group, enemy_group, hand):
     while pygame.mouse.get_pressed()[0]:
         pygame.time.Clock().tick(40)
         screen.blit(bg, (0, 0))
-        hand.draw_hand(screen)
-        player_group.draw(screen)
+        player.hand.draw(screen)
+        player.draw(screen)
         enemy_group.draw(screen)
         enemy_group.draw_rect(screen)
-        player_group.draw_rect(screen)
+        player.show_box(screen)
         mX, mY = pygame.mouse.get_pos()
         target.update(mX, mY)
-        # If a sucessful collisoin with the enemy is detected
-        if any([sp.collision(mX, mY) for sp in enemy_group]):
-            screen.blit(target.get_atk(), (mX, mY))
-        # If a sucessful collision with the player is detected
-        elif any([sp.collision(mX, mY) for sp in player_group]):
-            screen.blit(target.get_bst(), (mX, mY))
+        if card.ctype == 'TARGET_ATTACK':
+            # If a sucessful collisoin with the enemy is detected
+            if any([sp.collision(mX, mY) for sp in enemy_group]):
+                screen.blit(target.get_atk(), (mX, mY))
+            else:
+                screen.blit(target.get_img(), (mX, mY))
         else:
             screen.blit(target.get_img(), (mX, mY))
         pygame.event.pump()
@@ -39,40 +39,38 @@ def targeting(screen, bg, player_group, enemy_group, hand):
     pygame.mouse.set_visible(True)
     return
 
-def battle(screen, player_group):
+def battle(screen, player):
     background = pygame.transform.scale(pygame.image.load(
             os.path.join(ASSETS_PATH, "Background", "spacefield_a-000.png")),
             (SCREEN_WIDTH, SCREEN_HEIGHT))
-    enemy_group = Enemy()
+    enemy_group = Enemy_Gr()
     # change to dynamically create enemies
-    enemy0 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship4/Ship4.png'),
-        SCREEN_WIDTH - 100, SCREEN_HEIGHT / 2 + 50)
-    enemy1 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship2/Ship2.png'),
-        SCREEN_WIDTH - 200, SCREEN_HEIGHT / 2 - 50)
-    enemy2 = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship5/Ship5.png'),
-        SCREEN_WIDTH - 100, SCREEN_HEIGHT / 2)
+    enemy0 = Enemy(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship4/Ship4.png'), 30,
+        SCREEN_WIDTH - 200, SCREEN_HEIGHT / 2 -100)
+    enemy1 = Enemy(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship2/Ship2.png'), 20,
+        SCREEN_WIDTH - 150, SCREEN_HEIGHT / 2 - 200)
+    enemy2 = Enemy(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship5/Ship5.png'), 30,
+        SCREEN_WIDTH - 150, SCREEN_HEIGHT / 2)
     enemy_group.add(enemy0, enemy1, enemy2)
     for enemy in enemy_group:
         enemy.flip()
     enemy_group.update()
 
-    player_group.update()
-    hand = Hand()
+    player.update()
+    player.reset_decks()
+    player.draw_hand()
+    player.hand.draw(screen)
+    player.hand.position_hand()
     # change to draw function
-    hand.add(Card("C_Laser_Cannon.png", SCREEN_WIDTH / 2 - CARD__WIDTH / 2, SCREEN_HEIGHT - CARD_HEIGHT),
-             Card("C_Shield.png", SCREEN_WIDTH / 2 + CARD__WIDTH / 2, SCREEN_HEIGHT - CARD_HEIGHT))
-    hand.update()
     pygame.mouse.set_visible(True)
     while True:
         pygame.time.Clock().tick(40)
         # draw background
         screen.blit(background, (0, 0))
         # dynamic hand animation
-        hand.ddraw(screen, pygame.mouse.get_pos())
+        player.hand.ddraw(screen, pygame.mouse.get_pos())
         # place the player object (the loaded image)
-        player_group.draw(screen)
-        for player in player_group:
-            screen.blit(player.image, player.rect)
+        player.draw(screen)
         enemy_group.draw(screen)
 
         for event in pygame.event.get():
@@ -80,10 +78,10 @@ def battle(screen, player_group):
                 pygame.quit()
                 sys.exit()
             elif event.type == MOUSEBUTTONDOWN:
-                for card in hand:
+                for card in player.hand:
                     if card.rect.collidepoint(pygame.mouse.get_pos()):
                         card.highlight = True
-                        targeting(screen, background, player_group, enemy_group, hand)
+                        targeting(screen, background, card, player, enemy_group)
                         card.highlight = False
                         break
 
@@ -97,10 +95,11 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # declare the size of the map
 
-    player_group = Player()
-    player = Character(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship3/Ship3.png'),
-            0, 300)
-
-    player_group.add(player)
-
-    battle(screen, player_group)
+    player = Player(os.path.join(ASSETS_PATH, SHIPS_PATH, 'Ship3/Ship3.png'),
+            40, 0, SCREEN_HEIGHT / 3)
+    all_cards = cards.load_cards(os.path.join(ASSETS_PATH, CARD_PATH, 'cards.json'))
+    for _ in range(3):
+        player.all_cards.append(all_cards[0].copy())
+        player.all_cards.append(all_cards[1].copy())
+        player.all_cards.append(all_cards[2].copy())
+    battle(screen, player)
--- character.py
+++ character.py
@@ -2,20 +2,14 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pygame import sprite
+import random
+import cards
+from gamestate import *
 
-WHITE = (255, 255, 255)
 X = 0
 Y = 1
 
-class Player(sprite.Group):
-    def __init__(self):
-        super().__init__()
-
-    def draw_rect(self, screen):
-        for s in self.sprites():
-            s.show_box(screen)
-
-class Enemy(sprite.Group):
+class Enemy_Gr(sprite.Group):
     def __init__(self):
         super().__init__()
 
@@ -24,21 +18,31 @@ class Enemy(sprite.Group):
             s.show_box(screen)
 
 class Character(sprite.Sprite):
-    def __init__(self, image, x=0, y=0):
+    def __init__(self, image, max_health=40, x=0, y=0):
         super().__init__()
         self.image = pygame.image.load(image)
         self.pos = [x, y]
         self.rect = self.image.get_rect(topleft=self.pos)
-        self.rect2 = self.image.get_bounding_rect()
-        self.rect2.center = self.rect.center
+        self.bounding_rect = self.image.get_bounding_rect()
+        self.bounding_rect.center = self.rect.center
+        self.health_bar = self.health_init()
+        # stats
+        self.max_health = max_health
+        self.current_health = max_health
+        self.defense = 0
 
     def update(self):
         self.rect = self.image.get_rect(topleft=self.pos)
-        self.rect2 = self.image.get_bounding_rect()
-        self.rect2.center = self.rect.center
+        self.bounding_rect = self.image.get_bounding_rect()
+        self.bounding_rect.center = self.rect.center
+        self.health_init()
+
+    def health_init(self):
+        left, top, width, height = self.bounding_rect
+        self.health_bar = pygame.Rect((left, top + height, width, 8))
 
     def collision(self, x, y):
-        return self.rect2.collidepoint(x, y)
+        return self.bounding_rect.collidepoint(x, y)
 
     def rescale(self, w, h):
         self.image = pygame.transform.scale(self.image, (w, h))
@@ -60,7 +64,54 @@ class Character(sprite.Sprite):
         return self.image
 
     def show_box(self, screen):
-        pygame.draw.rect(screen, WHITE, self.rect2, 1)
+        pygame.draw.rect(screen, WHITE, self.bounding_rect, 1)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        self.draw_health(screen)
+
+    def draw_health(self, screen):
+        if self.defense:
+            pygame.draw.rect(screen, BLUE, self.health_bar)
+        else:
+            pygame.draw.rect(screen, GRAY, self.health_bar)
+            health = self.health_bar.copy()
+            health.width *= self.current_health / self.max_health
+
+class Player(Character):
+    def __init__(self, image, health, x=0, y=0):
+        super().__init__(image, health, x, y)
+        self.max_handsize = 3
+        self.all_cards = []
+        # battle hands
+        self.hand = cards.Hand()
+        self.graveyard = []
+        self.deck = []
+        self.in_play = []
+
+    def draw_hand(self):
+        for _ in range(self.max_handsize):
+            # If deck is empty reshuffle graveyard into deck before drawing cards
+            if not self.deck:
+                random.shuffle(self.graveyard)
+                self.deck = self.graveyard
+                self.graveyard = []
+            c = self.deck.pop()
+            self.hand.add(c)
+
+    def reset_decks(self):
+        self.hand.empty()
+        self.deck = self.all_cards.copy()
+        random.shuffle(self.deck)
+        self.graveyard = []
+        self.in_play = []
+
+class Enemy(Character):
+    def __init__(self, image, health, x, y):
+        super().__init__(image, health, x, y)
+        self.defense = 0
+        self.attacks = []
+        self.attack_idx = 0
 
 class Target(sprite.Sprite):
     def __init__(self):
